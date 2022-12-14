2416d6e4b86323851210ce20ea24710626e88f07
Yaroslav Yakupov <yakupov.yaroslav@icloud.com>
2022-12-14 15:09:45 +0300

    add some forms

---
+++ src/components/AccountForm.tsx
@@ -0,0 +1,8 @@
+export function AccountForm() {
+    return (<>
+        <label>Email</label>
+        <input autoFocus required type='email' />
+        <label>Password</label>
+        <input required type='email' />
+    </>)
+}
