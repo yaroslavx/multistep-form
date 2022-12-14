d14a0eecb00f75cef0b976168671911aa15a0055
Yaroslav Yakupov <yakupov.yaroslav@icloud.com>
2022-12-14 14:10:53 +0300

    add useMultistepForm hook

---
+++ src/hooks/useMultistepForm.ts
@@ -0,0 +1,25 @@
+import { ReactElement, useState } from 'react';
+
+export function useMultistepForm(steps: ReactElement[]) {
+  const [currentStepIndex, setCurrentStepIndex] = useState(0);
+
+  function next() {
+    setCurrentStepIndex((i) => (i >= steps.length - 1 ? 0 : i + 1));
+  }
+
+  function back() {
+    setCurrentStepIndex((i) => (i <= 0 ? steps.length - 1 : i - 1));
+  }
+
+  function goTo(index: number) {
+    setCurrentStepIndex(index);
+  }
+
+  return {
+    currentStepIndex,
+    step: steps[currentStepIndex],
+    next,
+    back,
+    goTo,
+  };
+}
